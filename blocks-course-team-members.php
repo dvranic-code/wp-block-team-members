da1ad536a17e2e499413506d63ae2f4602a08d0a
Dejan Rudic Vranic <dejanrudicvranic@gmail.com>
2024-02-19 15:25:57 +0100

    meta data change

--- blocks-course-team-members.php
+++ blocks-course-team-members.php
@@ -1,16 +1,16 @@
 <?php
 /**
- * Plugin Name:       Boilerplate
- * Description:       Example block written with ESNext standard and JSX support – build step required.
+ * Plugin Name:       Team Members
+ * Description:       A custom block for displaying team members.
  * Requires at least: 5.7
  * Requires PHP:      7.0
  * Version:           0.1.0
  * Author:            The WordPress Contributors
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       boilerplate
+ * Text Domain:       bc-team-members
  *
- * @package           create-block
+ * @package           blocks-course
  */
 
 /**
@@ -20,7 +20,7 @@
  *
  * @see https://developer.wordpress.org/block-editor/tutorials/block-tutorial/writing-your-first-block-type/
  */
-function create_block_boilerplate_block_init() {
-	register_block_type_from_metadata( __DIR__ );
+function create_block_team_members_init() {
+	register_block_type( __DIR__ );
 }
-add_action( 'init', 'create_block_boilerplate_block_init' );
+add_action( 'init', 'create_block_team_members_init' );
